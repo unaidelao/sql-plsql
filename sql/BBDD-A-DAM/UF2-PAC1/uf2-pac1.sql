27282b47e33ac84be91e2564efad2d1572b0b6f3
unaidelao <unai.delao@gmail.com>
2018-07-09 09:06:22 +0200

    Add uf2-pac1.sql

---
+++ sql/BBDD-A-DAM/UF2-PAC1/uf2-pac1.sql
@@ -0,0 +1,168 @@
+-- UF2 PAC01, BASES DE DATOS A
+-- Alumno: Unai de la O Pagaegui
+-- versión 2.0: realizadas mejoras al declarar los CONSTRAINT
+
+/* 
+ * Instrucciones DDL-SQL para crear la Base de Datos CAMPEONATO, según se indica
+ * en la descripción de la PAC01-UF02
+ */
+ 
+CREATE DATABASE IF NOT EXISTS CAMPEONATO;
+USE CAMPEONATO;
+
+CREATE TABLE IF NOT EXISTS HOTEL
+(
+  codigo_hotel CHAR(4) NOT NULL,
+  nombre_hotel VARCHAR(50),
+  direccion_hotel VARCHAR(100),
+  telefono_hotel CHAR(9),
+  localidad_hotel VARCHAR(50),
+  provincia_hotel VARCHAR(50),
+  CONSTRAINT PK_HOTEL PRIMARY KEY (codigo_hotel)
+);
+
+CREATE TABLE IF NOT EXISTS PAIS
+(
+  codigo_pais CHAR(4) NOT NULL,
+  nombre_pais VARCHAR(50),
+  continente_pais VARCHAR(10),
+  clubes_pais INT UNSIGNED,
+  CONSTRAINT PK_PAIS PRIMARY KEY (codigo_pais)
+);
+
+CREATE TABLE IF NOT EXISTS SALAS
+(
+  codigo_salas CHAR(4) NOT NULL,
+  nombre_sala VARCHAR(50),
+  capacidad INT UNSIGNED,
+  medio VARCHAR(20),
+  hotel CHAR(4),
+  CONSTRAINT FK_SALAS FOREIGN KEY (hotel) REFERENCES HOTEL (codigo_hotel),
+  CONSTRAINT PK_SALAS PRIMARY KEY (codigo_salas)
+);
+
+CREATE TABLE IF NOT EXISTS PARTICIPANTES
+(
+  num_asoc CHAR(3) NOT NULL,
+  nombre_part VARCHAR(50),
+  apellidos_part VARCHAR(50),
+  direccion_part VARCHAR(100),
+  telefono_part CHAR(9),
+  campeonatos INT UNSIGNED,
+  pais CHAR(4),
+  CONSTRAINT FK_PARTICIPANTES FOREIGN KEY (pais) REFERENCES PAIS (codigo_pais),
+  CONSTRAINT PK_PARTICIPANTES PRIMARY KEY (num_asoc)
+);
+
+CREATE TABLE IF NOT EXISTS ALOJAR
+(
+  num_asoc CHAR(3) NOT NULL,
+  hotel CHAR(4) NOT NULL,
+  fecha_in DATE,
+  fecha_out DATE,
+  CONSTRAINT FK_ALOJAR_1 FOREIGN KEY (num_asoc) REFERENCES PARTICIPANTES (num_asoc),
+  CONSTRAINT FK_ALOJAR_2 FOREIGN KEY (hotel) REFERENCES HOTEL (codigo_hotel),
+  CONSTRAINT PK_ALOJAR PRIMARY KEY (num_asoc,hotel)
+);
+
+CREATE TABLE IF NOT EXISTS JUGADORES
+(
+  num_asoc CHAR(3) NOT NULL,
+  nivel INT UNSIGNED,
+  CONSTRAINT FK_JUGADORES FOREIGN KEY (num_asoc) REFERENCES PARTICIPANTES (num_asoc),
+  CONSTRAINT PK_JUGADORES PRIMARY KEY (num_asoc)
+);
+
+CREATE TABLE IF NOT EXISTS ARBITROS
+(
+  num_asoc CHAR(3) NOT NULL,
+  CONSTRAINT FK_ARBITROS FOREIGN KEY (num_asoc) REFERENCES PARTICIPANTES (num_asoc),
+  CONSTRAINT PK_ARBITROS PRIMARY KEY (num_asoc)
+);
+
+CREATE TABLE IF NOT EXISTS PARTIDA
+(
+  codigo_partida CHAR(5) NOT NULL,
+  arbitro cHAR(3),
+  codigo_salas CHAR(4),
+  CONSTRAINT FK_PARTIDA_1 FOREIGN KEY (arbitro) REFERENCES ARBITROS (num_asoc),
+  CONSTRAINT FK_PARTIDA_2 FOREIGN KEY (codigo_salas) REFERENCES SALAS (codigo_salas),
+  CONSTRAINT PK_PARTIDA PRIMARY KEY (codigo_partida)
+);
+
+CREATE TABLE IF NOT EXISTS REPRESENTA
+(
+  pais_1 CHAR(4) NOT NULL,
+  pais_2 CHAR(4) NOT NULL,
+  CONSTRAINT FK_REPRESENTA_1 FOREIGN KEY (pais_1) REFERENCES PAIS (codigo_pais),
+  CONSTRAINT FK_REPRESENTA_2 FOREIGN KEY (pais_2) REFERENCES PAIS (codigo_pais),
+  CONSTRAINT PK_REPRESENTA PRIMARY KEY (pais_1, pais_2)
+);
+
+CREATE TABLE IF NOT EXISTS JUGAR
+(
+  num_asoc CHAR(3) NOT NULL,
+  codigo_partida CHAR(5) NOT NULL,
+  color_ficha ENUM ('blancas', 'negras'), -- porque solo puede ser blancas o negras
+  CONSTRAINT FK_JUGAR_1 FOREIGN KEY (num_asoc) REFERENCES PARTICIPANTES (num_asoc),
+  CONSTRAINT FK_JUGAR_2 FOREIGN KEY (codigo_partida) REFERENCES PARTIDA (codigo_partida),
+  CONSTRAINT PK_JUGAR PRIMARY KEY (num_asoc, codigo_partida)
+);
+
+/*
+ * Instrucciones para INSERTAR datos en la base de datos creada CAMPEONATO,
+ * según los requisitos de la PAC01-UF2.
+ */
+ 
+ INSERT INTO HOTEL VALUES ('h001', 'hotel central', 'c/ larga,7', '923230101', 'Lleida', 'Lleida');
+ INSERT INTO HOTEL VALUES ('h002', 'hotel cappont', 'c/ rio ebro,7', '923555657', 'Alcarras', 'Lleida');
+ INSERT INTO HOTEL VALUES ('h003', 'hotel ilerna', 'avda barcelona,13', '922211001', 'Barcelona', 'Barcelona');
+ INSERT INTO HOTEL VALUES ('h004', 'hotel real', 'c/ ancha,14', '923622558', 'Lleida', 'Lleida');
+ INSERT INTO HOTEL VALUES ('h005', 'hostal manzanares', 'avda mayor,30', '923455221', 'Bellpuig', 'Lleida');
+ 
+ INSERT INTO PAIS VALUES ('p001', 'Rusia', 'Europa', 10);
+ INSERT INTO PAIS VALUES ('p002', 'Francia', 'Europa', 3);
+ INSERT INTO PAIS VALUES ('p003', 'Guayana Francesa', 'America', 1);
+ INSERT INTO PAIS VALUES ('p004', 'Uzbekistan', 'Asia', 8);
+ INSERT INTO PAIS VALUES ('p005', 'Nigeria', 'Africa', 14);
+ 
+ INSERT INTO SALAS VALUES ('s001', 's_euclides', 30, 'todos', 'h001');
+ INSERT INTO SALAS VALUES ('s002', 's_descartes', 60, 'todos', 'h001');
+ INSERT INTO SALAS VALUES ('s003', 's_principal', 15, 'video', 'h003');
+ INSERT INTO SALAS VALUES ('s004', 's_gerona', 30, 'audio', 'h004');
+ INSERT INTO SALAS VALUES ('s005', 's_tarragona', 80, 'todos', 'h004');
+ 
+ INSERT INTO PARTICIPANTES VALUES ('001', 'JOSE LUIS', 'LOPEZ VAZQUEZ', 'c/guadalquivir,8', '955233221', 4, 'p001');
+ INSERT INTO PARTICIPANTES VALUES ('002', 'SERGIO', 'RAMOS GARCIA', 'c/victoria,18', '955233441', 6, 'p003');
+ INSERT INTO PARTICIPANTES VALUES ('003', 'MICKEY', 'MOUSE LOPEZ', 'c/disney,28', '988566998', 1, 'p002');
+ INSERT INTO PARTICIPANTES VALUES ('004', 'DONALD JOSE', 'PATO GARCIA', 'c/gargolas,14', '988744552', 14, 'p004');
+ INSERT INTO PARTICIPANTES VALUES ('005', 'LUIS BRAD', 'PITT JACKSON', 'avda america,1', '652255887', 4, 'p005');
+ 
+ INSERT INTO ALOJAR VALUES ('001', 'h001', '2017-06-26', '2017-06-28');
+ INSERT INTO ALOJAR VALUES ('002', 'h001', '2017-06-26', '2017-07-26');
+ INSERT INTO ALOJAR VALUES ('003', 'h002', '2017-06-26', '2017-07-26');
+ INSERT INTO ALOJAR VALUES ('004', 'h003', '2017-06-26', '2017-07-01');
+ INSERT INTO ALOJAR VALUES ('005', 'h005', '2017-06-26', '2017-07-14');
+ 
+ INSERT INTO JUGADORES VALUES ('001', 5);
+ INSERT INTO JUGADORES VALUES ('002', 2);
+ INSERT INTO JUGADORES VALUES ('003', 6);
+  
+ INSERT INTO ARBITROS VALUES ('004');
+ INSERT INTO ARBITROS VALUES ('005');
+ 
+ INSERT INTO PARTIDA VALUES ('par01', '004', 's001');
+ INSERT INTO PARTIDA VALUES ('par02', '004', 's002');
+ INSERT INTO PARTIDA VALUES ('par03', '005', 's001');
+ INSERT INTO PARTIDA VALUES ('par04', '005', 's005');
+ INSERT INTO PARTIDA VALUES ('par05', '004', 's003');
+ 
+ INSERT INTO REPRESENTA VALUES ('p002', 'p003');
+ INSERT INTO REPRESENTA VALUES ('p001', 'p004');
+ 
+ INSERT INTO JUGAR VALUES ('001', 'par01', 'negras');
+ INSERT INTO JUGAR VALUES ('001', 'par03', 'negras');
+ INSERT INTO JUGAR VALUES ('002', 'par01', 'blancas');
+ INSERT INTO JUGAR VALUES ('002', 'par02', 'negras');
+ INSERT INTO JUGAR VALUES ('003', 'par02', 'blancas');
+ INSERT INTO JUGAR VALUES ('003', 'par03', 'blancas');
